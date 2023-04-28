abd16951d49bf2f324ac0cb4b00592dbce75db5c
Ablanore <erick@ozouf.com>
2023-04-28 17:12:12 +0200

    poussge des datas

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,14 @@
 
 //#endregion import des Class
 
+//Terrain de jeu
+var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+
+
+
+
 export function maFunction() {
 
 }
